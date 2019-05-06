7202c5016cc4bcb98bc248afb4a409ad3470cb11
Eclipse <santiago.paiz@gmail.com>
2019-05-06 11:17:42 -0600

    nop

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from Library.utils import *
 
 
-# print(len(readElements('Circle')))
+print(len(readElements('Circle')))
 # print(len(readElements('Egg')))
 # print(len(readElements('House')))
 # print(len(readElements('MickeyMouse')))
-print(len(readElements('QuestionMark')))
+# print(len(readElements('QuestionMark')))
 # print(len(readElements('SadFace')))
 # print(len(readElements('Square')))
 # print(len(readElements('Tree')))
